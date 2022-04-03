0a9305e20fadc2f8949386ac537c97ec903d7091
Raj Aryan <73771884+rajaryan18@users.noreply.github.com>
2022-04-03 12:52:06 +0530

    brownie set-up

---
+++ scripts/frontend_scripts.py
@@ -0,0 +1,17 @@
+import yaml, json, os, shutil
+
+def update_frontend():
+    copy_folders_to_frontend("./build", "./frontend/src/chain-info")
+    with open("brownie-config.yaml", "r") as brownie_config:
+        config_dict = yaml.load(brownie_config, Loader=yaml.FullLoader)
+        with open("./frontend/src/brownie-config.json", "w") as bc:
+            json.dump(config_dict, bc)
+    print("Frontend Updated")
+
+def copy_folders_to_frontend(src, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    shutil.copytree(src, dest)
+
+def main():
+    update_frontend()
